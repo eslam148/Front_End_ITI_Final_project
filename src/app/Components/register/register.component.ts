e78e82d4254e41755c899befd06ebdc2a350c605
ahmedthabet97 <ahmedaboaouf333@gmail.com>
2022-12-04 15:35:47 +0200

    add login ,register and about us Component

---
+++ src/app/Components/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  constructor(public translate:TranslateService){}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,12 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, CartComponent, AboutUsComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
