33ef42f8331cdec8fc80b3d753677c5d059ae617
islam12111998 <islam.18333388@compit.aun.edu.eg>
2022-12-08 16:29:31 +0200

    add payment

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../Services/product.service';
 import { IProduct } from './../../Model/IProduct';
 import { CartService } from '../../Services/cart.service';
+import {ICreateOrderRequest, IPayPalConfig} from 'ngx-paypal';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
   productInCart: IProduct[] = [];
-  totalPrice!:number
+  totalPrice!: number;
+  public payPalConfig?: IPayPalConfig;
   constructor(
     private ProductService: ProductService,
     private CartService: CartService
@@ -18,5 +20,68 @@ export class CartComponent {
     this.productInCart = this.CartService.getItems();
     this.totalPrice = this.CartService.totalPrice();
   }
+  ngOnInit(): void {
+   this.initConfig();
+  }
 
+ private initConfig(): void {
+    this.payPalConfig = {
+      currency: 'EUR',
+      clientId: 'AToDsg4D_1Rdhkp5gEA71rJcqvKLRahIV5DEC2wSVFdAkz6XdYODv2GsHzqx0UbqPCi-A3xLKSVabQX6', // add paypal clientId here
+      createOrderOnClient: (data) => <ICreateOrderRequest> {
+        intent: 'CAPTURE',
+        purchase_units: [{
+          amount: {
+            currency_code: 'EUR',
+            value: '0.01',
+            breakdown: {
+              item_total: {
+                currency_code: 'EUR',
+                value: '0.01'
+              }
+            }
+          },
+          items: [{
+            name: 'The Swag Coder',
+            quantity: '1',
+            category: 'DIGITAL_GOODS',
+            unit_amount: {
+              currency_code: 'EUR',
+              value: '0.01',
+            },
+          }]
+        }]
+      },
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        label: 'paypal',
+        layout: 'vertical',
+
+        color: 'blue',
+        shape: 'rect'
+      },
+      onApprove: (data, actions) => {
+        console.log('onApprove - transaction was approved, but not authorized', data, actions);
+        actions.order.get().then((details: any) => {
+          console.log('onApprove - you can get full order details inside onApprove: ', details);
+        });
+
+      },
+      onClientAuthorization: (data) => {
+        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+
+      },
+      onError: err => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      }
+    };
+  }
 }
